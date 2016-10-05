e2b9dd22bd673366860cf5e3fe16e9a7905c316c
infiniti008 <nikitenok.sl@gmail.com>
2016-10-05 19:47:15 +0300

    Определяю номер сезона и серии

--- parser.js
+++ parser.js
@@ -10,8 +10,18 @@ function parseFrom(config, cb){
 		var res = {};
 		for (var i in config.selectors){
 			res[i] = $(config.selectors[i]).text();
+			if (i == 'SeasonInfo') {
+				var CurrentSeason = {};
+				CurrentSeason.IndexSeason = res[i].match('сезон').index;
+				CurrentSeason.NumberSeason = (res[i].charAt(CurrentSeason.IndexSeason-3) + res[i].charAt(CurrentSeason.IndexSeason-2) + res[i].charAt(CurrentSeason.IndexSeason-1)) * 1;
+				CurrentSeason.IndexEpisode = res[i].match('серия').index;
+				CurrentSeason.NumberEpisode = (res[i].charAt(CurrentSeason.IndexEpisode-3) + res[i].charAt(CurrentSeason.IndexEpisode-2) + res[i].charAt(CurrentSeason.IndexEpisode-1)) * 1;
+			}
 		}
+		console.log(CurrentSeason);
 		res.create = new Date();
+
+
 		cb(res);
 	});
 }
